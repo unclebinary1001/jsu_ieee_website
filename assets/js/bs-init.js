8ca71efcdcab74458161f9c70f17daf808cc7273
Khaylah Rose <khaylahrscholar@gmail.com>
2022-05-13 20:37:53 -0400

    Updated Home Css and about page

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -10,3 +10,16 @@ if (window.innerWidth < 768) {
 document.addEventListener('DOMContentLoaded', function() {
 	AOS.init();
 }, false);
+
+/*const observer = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('shring-animations');
+    }
+  });
+});
+
+observer.observe(document.querySelector('.dox'));*/
